077607bfb5d0a90aaa3d57de07c79a1a05697d6a
PlatinumMaster <ikeita@umass.edu>
2022-09-30 00:41:16 -0400

    Initial video capture, Z-axis control.
    	modified:   README.md
    	modified:   s5c-py/DroneCtrl.py
    	modified:   s5c-py/TCPStream.py
    
    	deleted:    gyeh.mp4
    	deleted:    s5c-py/DrakeFan.py
    	deleted:    strm.h264

--- s5c-py/DroneCtrl.py
+++ s5c-py/DroneCtrl.py
@@ -1,15 +1,17 @@
+from xml.dom.expatbuilder import theDOMImplementation
 from TCPStream import *
 from UDPStream import *
 import pygame
 from pygame.locals import *
+import threading
 
 TakeoffEnabled = False
 Delta = {
-    'Flag' : 0x0,
-    'X' : 0x0,
-    'Y' : 0x0,
-    'Z' : 0x0,
-    'Rotation' : 0x0
+    'Flag': 0x0,
+    'X': 0x0,
+    'Y': 0x0,
+    'Z': 0x0,
+    'Rotation': 0x0
 }
 Host = '172.17.10.1'
 Ports = {
@@ -29,24 +31,56 @@ print('[+] Connecting to UDP...')
 UDP.Connect(Host, Ports['UDP'])
 print('[+] Connected to UDP.')
 
-Range = 0x30
-Z_Range = 0x10
-pygame.init()
-pygame.joystick.init()
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
 
-while True:
-    Delta['X'] = int(joystick.get_axis(0) * Range)
-    Delta['Y'] = -int(joystick.get_axis(1) * Range)
-    Delta['Z'] = int(joystick.get_axis(5) * Z_Range) - int(joystick.get_axis(2) * Z_Range) if joystick.get_axis(5) > 0.0 or joystick.get_axis(2) > 0.0 else 0
-    Delta['Flag'] = 0x1 if joystick.get_button(9) else 0x0
-    
-    if Delta['Flag'] is 0x1:
-        TakeoffEnabled = True
-    if TakeoffEnabled:
-        UDP.SendCommand(Delta['Flag'], 0x80 + Delta['X'], 0x80 + Delta['Y'], 0x80 + Delta['Z'], 0x80 + Delta['Rotation'])
-        
- 
+def VideoStreamHandler():
+    with open('strm.h264', 'wb') as strm:
+        while True:
+            strm.write(TCP.Receive(0x400))
 
 
+def ControlSystemHandler():
+    Ranges = {
+        'XY' : 0x30,
+        'Z' : 0x10
+    }
+
+    pygame.init()
+    pygame.joystick.init()
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+
+    TakeoffEnabled = False
+
+    while True:
+        pygame.event.get()
+        Delta['Flag'] = 0x1 if joystick.get_button(9) else 0x0
+
+        if Delta['Flag'] is 0x1:
+            TakeoffEnabled = True
+
+        if joystick.get_button(6):
+            Delta['Z'] = -0x80
+            Delta['Flag'] = 0x0
+            TakeoffEnabled = False
+            EndTime = time.time() + 5
+            while time.time() < EndTime:
+                UDP.SendCommand(Delta['Flag'], 0x80 + Delta['X'], 0x80 +
+                                Delta['Y'], 0x80 + Delta['Z'], 0x80 + Delta['Rotation'])
+
+        if TakeoffEnabled:
+            Delta['X'] = int(joystick.get_axis(0) * Ranges['XY'])
+            Delta['Y'] = -int(joystick.get_axis(1) * Ranges['XY'])
+            Delta['Z'] = 0
+            if joystick.get_axis(5) > 0.0:
+                Delta['Z'] += int(joystick.get_axis(5) * Ranges['Z'])
+            if joystick.get_axis(2) > 0.0:
+                Delta['Z'] -= int(joystick.get_axis(2) * Ranges['Z'])
+            UDP.SendCommand(Delta['Flag'], 0x80 + Delta['X'], 0x80 +
+                            Delta['Y'], 0x80 + Delta['Z'], 0x80 + Delta['Rotation'])
+
+
+VideoThread = threading.Thread(target=VideoStreamHandler, args=())
+ControlThread = threading.Thread(target=ControlSystemHandler, args=())
+
+VideoThread.start()
+ControlThread.start()
